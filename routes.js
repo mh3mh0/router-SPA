b18577d1cc02b0d43a565c18d8bfbc88dcf6b7c3
mh3mh0 <jonxca@icloud.com>
2021-04-27 09:17:57 +0200

    first commit

---
+++ routes.js
@@ -0,0 +1,10 @@
+const routes = [
+  { path: "/", template: "<h1>Hola</h1>" },
+
+  {
+    path: "/contact",
+    template: "<h1>Contact to my email jonxca@icloud.com</h1>",
+  },
+
+  { path: "/aboutme", template: "<h1>ABOUT ME</h1> " },
+];
